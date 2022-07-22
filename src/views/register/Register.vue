5da86db615bc4863f72220858b0f67d7fcd7cfcd
nshcn <2695563464@qq.com>
2022-07-22 14:03:08 +0800

    表单验证

--- src/views/register/Register.vue
+++ src/views/register/Register.vue
@@ -11,15 +11,7 @@
                class="demo-ruleForm">
         <el-form-item label="手机号"
                       prop="phoneNum"
-                      ref="phoneNum"
-                      :rule="[
-          { required: true, message: '手机号不能为空！', trigger: 'blur' },
-          { min: 11, max: 11, message: '手机号长度错误', trigger: 'blur' },
-          {
-            pattern: /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/,
-            message: '请输入正确的手机号码'
-          }
-        ]">
+                      ref="phoneNum">
           <el-input v-model="ruleForm.phoneNum"></el-input>
         </el-form-item>
         <el-form-item label="验证码"
@@ -104,10 +96,7 @@ export default {
   },
   methods: {
     getKaptcha(phoneNum) {
-      console.log('在', this.$refs[phoneNum])
-      this.$refs[phoneNum].validate((valid) => {
-        this.loading = true // 提交按钮显示加载动画
-        console.log(valid)
+      this.$refs['ruleForm'].validateField('phoneNum', (valid) => {
         if (valid) {
           let _this = this
           this.axios({
